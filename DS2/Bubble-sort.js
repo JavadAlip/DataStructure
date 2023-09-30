cf927482d772708563c34bc5d8945c27e4c685e7
javadAlip <javadalipp911@gmail.com>
2023-10-01 00:50:11 +0530

    DS-2

---
+++ DS2/Bubble-sort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr){
+    for(i=0;i<arr.length-1;i++){
+        for(j=0;j<arr.length-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                let temp=arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=temp
+            }
+        }
+    }
+    return arr
+}
+const arr=[1,2,1,2,7,5]
+console.log(bubbleSort(arr))
