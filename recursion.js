7afc88faac466d3dea07c435a864a8d6eae6fb82
javadAlip <javadalipp911@gmail.com>
2023-10-01 00:31:57 +0530

    Find a factorial-DS1

---
+++ recursion.js
@@ -0,0 +1,30 @@
+function a(n){
+    if (n <= 1){
+        return 1;
+    } 
+    return n * a(n - 1);
+}
+console.log(a(6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+function a(n){
+    if(n<=1){
+        return 
+    }
+    a(n-1)
+    console.log(n);
+    a(n-1)
+}
+console.log(a(3));
+
