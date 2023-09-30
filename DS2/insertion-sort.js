97656a38e9de99bbe702c229f4558721f5a751f9
javadAlip <javadalipp911@gmail.com>
2023-10-01 01:04:05 +0530

    DS-2

---
+++ DS2/insertion-sort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr){
+    for(i=0;i<arr.length;i++){
+        let current=arr[i]
+        let j=i-1
+        while(j>=0 &&arr[j]>current){
+            arr[j+1]=arr[j]
+            j--
+        }
+        arr[j+1]=current 
+    }
+    return arr
+}
+let arr=[5,4,3,2,1]
+console.log(insertionSort(arr))
